9b06ba58d9a291eee245cab4b51292a3d988e827
Lorna Maria <lorna1661maria@gmail.com>
2023-02-08 01:20:28 +0000

    move files

--- app/src/read.py
+++
@@ -1,9 +0,0 @@
-"""
-Function to read file(csv) into application based on file type
-"""
-import pandas as pd
-
-def create_data_object(file:str):
-    """Created data object depending on file type"""
-    data_object = pd.read_csv(file)
-    return data_object
